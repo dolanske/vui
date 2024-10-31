59200bac8ac011fa413b186841c69ec6c5bc5aac
Jan Dolanský <dolanovsky@gmail.com>
2024-11-01 00:24:22 +0200

    wip

---
+++ src/components/Progress/Progress.vue
@@ -0,0 +1,46 @@
+<script setup lang='ts'>
+import { onBeforeUnmount, onMounted } from 'vue'
+
+interface Props {
+  /**
+   * Will randomly increment but never actually reach the end
+   */
+  fake?: boolean
+
+  /**
+   * Show the progress number as perceetnage
+   */
+  label?: boolean
+  /**
+   *
+   */
+  labelPosition?: 'left' | 'right' | 'center'
+}
+
+const {
+  fake,
+  label,
+  labelPosition = 'center',
+} = defineProps<Props>()
+const progress = defineModel<number>()
+
+let interval
+
+onMounted(() => {
+  if (fake) {
+    interval = setInterval(() => {
+      if (Math.random() > 0.5) {}
+    }, 1000)
+  }
+})
+
+onBeforeUnmount(() => {
+  clearInterval(interval)
+})
+</script>
+
+<template>
+  <div class="vui-progress-bar">
+    <div class="vui-progress-indicator" />
+  </div>
+</template>
