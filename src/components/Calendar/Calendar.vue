d7c74c3cb1b7683ed97f4d24679ddfda974cd055
Jan Dolanský <dolanovsky@gmail.com>
2024-11-05 23:59:23 +0200

    Add styles and default props

--- src/components/Calendar/Calendar.vue
+++ src/components/Calendar/Calendar.vue
@@ -1,17 +1,59 @@
 <script setup lang='ts'>
+import type { VueDatePickerProps } from '@vuepic/vue-datepicker'
+import { Icon } from '@iconify/vue'
 import VueDatePicker from '@vuepic/vue-datepicker'
+import { PropType, useAttrs } from 'vue'
+import Button from '../Button/Button.vue'
 import '@vuepic/vue-datepicker/dist/main.css'
 import './calendar.scss'
 
-// TODO: set default styles
-// https://vue3datepicker.com/customization/theming/
-
-// TODO: customize for generic usage
+const props = withDefaults(defineProps<VueDatePickerProps & {
+  expand?: boolean
+}>(), {
+  autoApply: true,
+  autoPosition: true,
+  dark: true,
+  format: 'dd/MM/yyyy HH:mm',
+  expand: false,
+})
 </script>
 
 <template>
-  <VueDatePicker auto-apply>
-    <template #trigger>
+  <VueDatePicker
+    v-bind="props"
+    :style="props.expand || !$slots.trigger
+      ? undefined
+      : { display: 'inline-block', width: 'auto' }
+    "
+  >
+    <!-- Icon slots -->
+    <template #input-icon>
+      <Icon :width="18" :height="18" icon="ph:calendar-blank" />
+    </template>
+    <template #calendar-icon>
+      <Icon :width="18" :height="18" icon="ph:calendar-blank" />
+    </template>
+    <template #clear-icon>
+      <Icon :width="18" :height="18" icon="ph:x" />
+    </template>
+    <template #clock-icon>
+      <Icon :width="18" :height="18" icon="ph:clock" />
+    </template>
+    <template #arrow-left>
+      <Icon :width="18" :height="18" icon="ph:caret-left" />
+    </template>
+    <template #arrow-right>
+      <Icon :width="18" :height="18" icon="ph:caret-right" />
+    </template>
+    <template #arrow-up>
+      <Icon :width="18" :height="18" icon="ph:caret-up" />
+    </template>
+    <template #arrow-down>
+      <Icon :width="18" :height="18" icon="ph:caret-down" />
+    </template>
+
+    <!-- Content slots -->
+    <template v-if="$slots.trigger" #trigger>
       <slot name="trigger" />
     </template>
   </VueDatePicker>
