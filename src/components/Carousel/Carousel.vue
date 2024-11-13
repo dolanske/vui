c91cc48308a22ed8f769fccf7bd86740a58dd402
Jan Dolanský <dolanovsky@gmail.com>
2024-11-14 00:04:18 +0200

    wip

---
+++ src/components/Carousel/Carousel.vue
@@ -0,0 +1,7 @@
+<script setup lang='ts'>
+
+</script>
+
+<template>
+  <div class="vui-carousel" />
+</template>
---
+++ src/components/Carousel/CarouselItem.vue
@@ -0,0 +1,11 @@
+<script setup lang='ts'>
+import { ref } from 'vue'
+
+const active = ref(0)
+</script>
+
+<template>
+  <div class="vui-carousel-item">
+    <div class="vui-carousel-wrapper" :style="{}" />
+  </div>
+</template>
