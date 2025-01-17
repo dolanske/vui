40fa455822bbf8a4317be92e1a2b0ee23e28953b
dolanske <dolanovsky@gmail.com>
2025-01-17 20:53:16 +0200

    Fix table, add more button examples

--- src/examples/ExampleButtons.vue
+++ src/examples/ExampleButtons.vue
@@ -6,49 +6,107 @@ const variants = ['default', 'danger', 'success', 'link', 'blue', 'accent'] as c
 </script>
 
 <template>
-  <ul class="flex flex-col g-l">
-    <li>
-      <h4 class="mb-s">
-        Base
-      </h4>
-      <Flex gap="xs">
-        <Button v-for="btn in variants" :key="btn" :variant="btn">
-          Button
-        </Button>
-      </Flex>
-    </li>
-    <li>
-      <h4 class="mb-s">
-        Active
-      </h4>
-      <Flex gap="xs">
-        <Button v-for="btn in variants" :key="btn" :variant="btn" active>
-          Button
-        </Button>
-      </Flex>
-    </li>
-    <li>
-      <h4>
-        Plain
-      </h4>
-      <p class="mb-l text-color-lighter">
-        Buttons are plain by default, but their hover state is by varaiant
-      </p>
-      <Flex gap="xs">
-        <Button v-for="btn in variants" :key="btn" :variant="btn" plain>
-          Button
-        </Button>
-      </Flex>
-    </li>
-    <li>
-      <h4 class="mb-s">
-        Disabled
-      </h4>
-      <Flex gap="xs">
-        <Button v-for="btn in variants" :key="btn" :variant="btn" disabled>
-          Button
-        </Button>
-      </Flex>
-    </li>
-  </ul>
+  <div class="mb-xxl">
+    <h3 class="mb-l">
+      Buttons
+    </h3>
+    <table>
+      <tbody>
+        <tr>
+          <th>Base</th>
+          <td>
+            <Flex gap="s">
+              <Button v-for="btn in variants" :key="btn" :variant="btn">
+                Button
+              </Button>
+            </Flex>
+          </td>
+        </tr>
+        <tr>
+          <th>Base + large</th>
+          <td>
+            <Flex gap="s">
+              <Button v-for="btn in variants" :key="btn" :variant="btn" size="l">
+                Button
+              </Button>
+            </Flex>
+          </td>
+        </tr>
+        <tr>
+          <th>Base + small</th>
+          <td>
+            <Flex gap="s">
+              <Button v-for="btn in variants" :key="btn" :variant="btn" size="s">
+                Button
+              </Button>
+            </Flex>
+          </td>
+        </tr>
+        <tr>
+          <th>Active</th>
+          <td>
+            <Flex gap="s">
+              <Button v-for="btn in variants" :key="btn" :variant="btn" active>
+                Button
+              </Button>
+            </Flex>
+          </td>
+        </tr>
+        <tr>
+          <th>Plain</th>
+          <td>
+            <Flex gap="s">
+              <Button v-for="btn in variants" :key="btn" :variant="btn" plain>
+                Button
+              </Button>
+            </Flex>
+          </td>
+        </tr>
+        <tr>
+          <th>Dashed</th>
+          <td>
+            <Flex gap="s">
+              <Button v-for="btn in variants" :key="btn" :variant="btn" dashed>
+                Button
+              </Button>
+            </Flex>
+          </td>
+        </tr>
+        <tr>
+          <th>Disabled</th>
+          <td>
+            <Flex gap="s">
+              <Button v-for="btn in variants" :key="btn" :variant="btn" disabled>
+                Button
+              </Button>
+            </Flex>
+          </td>
+        </tr>
+        <tr>
+          <th>Squar + icon</th>
+          <td>
+            <Flex gap="s">
+              <Button v-for="btn in variants" :key="btn" :variant="btn" square icon="ph:info" />
+            </Flex>
+          </td>
+        </tr>
+        <tr>
+          <th>Squar + icon + small</th>
+          <td>
+            <Flex gap="s">
+              <Button v-for="btn in variants" :key="btn" :variant="btn" square icon="ph:info" size="s" />
+            </Flex>
+          </td>
+        </tr>
+        <tr>
+          <th>Squar + icon + large</th>
+          <td>
+            <Flex gap="s">
+              <Button v-for="btn in variants" :key="btn" :variant="btn" square icon="ph:info" size="l" />
+            </Flex>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
 </template>
