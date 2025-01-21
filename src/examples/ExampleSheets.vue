f4348ff1e8cba3abddf945948a5ecfb01fb0fb49
Jan Dolanský <dolanovsky@gmail.com>
2025-01-22 00:12:37 +0200

    init sheet example 2

--- src/examples/ExampleSheets.vue
+++ src/examples/ExampleSheets.vue
@@ -1,7 +1,12 @@
 <script setup lang='ts'>
 import { ref } from 'vue'
+import Button from '../components/Button/Button.vue'
+import Select from '../components/Select/Select.vue'
+import Sheet from '../components/Sheet/Sheet.vue'
 
-const leftOpen = ref()
+const open = ref()
+const options = [{ value: 'left', label: 'Left' }, { value: 'right', label: ' Right' }, { value: 'top', label: 'Top' }, { value: 'bottom', label: 'Bottom' }]
+const option = ref(options[0])
 </script>
 
 <template>
@@ -14,7 +19,18 @@ const leftOpen = ref()
       <tbody>
         <tr>
           <th>Base</th>
-          <td />
+          <td>
+            <Select v-model="option" :options />
+            <Button @click="open = !open">
+              Open
+            </Button>
+            <Sheet v-model="open">
+              <template #header>
+                <h2>Hello </h2>
+              </template>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus alias consectetur obcaecati recusandae debitis voluptatum exercitationem. Nam facilis laudantium labore sint cum, veritatis autem asperiores consectetur possimus cumque beatae eveniet.</p>
+            </Sheet>
+          </td>
         </tr>
       </tbody>
     </table>
