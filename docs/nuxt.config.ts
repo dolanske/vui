7a6dc89f5acf666c589e044c66c383897b734a4c
Jan Dolanský <dolanovsky@gmail.com>
2025-07-06 21:44:37 +0300

    Update yaml?

--- docs/nuxt.config.ts
+++ docs/nuxt.config.ts
@@ -1,6 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   // ssr: false,
+  app: {
+    baseURL: '/docs',
+  },
   compatibilityDate: '2024-11-01',
   devtools: { enabled: false },
   css: [
