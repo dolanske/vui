195cc1d85093ebcf7fd4f0b0b60db76b8994494d
dolanske <dolanovsky@gmail.com>
2026-05-12 19:50:50 +0300

    wip

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,16 @@
 <script setup lang="ts">
+import ExampleAlerts from './examples/ExampleAlerts.vue'
 import ExampleBadges from './examples/ExampleBadges.vue'
-import ExampleCarousels from './examples/ExampleCarousels.vue'
-import ExampleCommands from './examples/ExampleCommands.vue'
+import ExampleButtons from './examples/ExampleButtons.vue'
 import { setColorTheme } from './shared/theme'
 
-setColorTheme('dark')
+setColorTheme('light')
 </script>
 
 <template>
   <main vaul-drawer-wrapper class="container-m py-xxxl">
     <ExampleBadges />
-    <ExampleCommands />
-    <ExampleCarousels />
+    <ExampleAlerts />
+    <ExampleButtons />
   </main>
 </template>
