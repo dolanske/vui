129276b463969213b6704f398b2e171aa5d44838
dolanske <dolanovsky@gmail.com>
2026-05-13 20:47:56 +0300

    Replace embed image with local copy

--- docs/components/content/AspectRatioExample.vue
+++ docs/components/content/AspectRatioExample.vue
@@ -7,7 +7,7 @@ import { AspectRatio, Flex } from '@dolanske/vui'
     <template #component>
       <Flex gap="l" column>
         <AspectRatio :ratio="16 / 9">
-          <img src="https://www.dropbox.com/scl/fi/1p7hn45t06di5oc9q8i3k/DSCF8128.JPG?rlkey=d1haqywuvo4h3tz8qqbdwh4ti&st=pwwqgy1n&raw=1" alt="16 / 9">
+          <img src="/example.jpg" alt="16 / 9">
         </AspectRatio>
       </Flex>
     </template>
