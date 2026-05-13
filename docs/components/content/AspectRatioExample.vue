bbde9c131210dfc59f2d624a19b68c0a201d01c5
dolanske <dolanovsky@gmail.com>
2026-05-13 21:35:06 +0300

    Fix build 2

--- docs/components/content/AspectRatioExample.vue
+++ docs/components/content/AspectRatioExample.vue
@@ -1,7 +1,5 @@
 <script setup lang="ts">
 import { AspectRatio, Flex } from '@dolanske/vui'
-
-const exampleImg = '/example.jpg'
 </script>
 
 <template>
@@ -9,7 +7,7 @@ const exampleImg = '/example.jpg'
     <template #component>
       <Flex gap="l" column>
         <AspectRatio :ratio="16 / 9">
-          <img :src="exampleImg" alt="16 / 9">
+          <NuxtImg src="/example.jpg" alt="16 / 9" />
         </AspectRatio>
       </Flex>
     </template>
