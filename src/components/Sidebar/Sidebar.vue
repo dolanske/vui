d620f3c5932d6c4ba1dbc74a178ed965d60845b7
dolanske <dolanovsky@gmail.com>
2026-09-21 20:20:35 +0300

    wip

--- src/components/Sidebar/Sidebar.vue
+++ src/components/Sidebar/Sidebar.vue
@@ -1,49 +1,48 @@
 <script setup lang='ts'>
 import { onClickOutside, useCssVar, useMouseInElement, useTimeoutFn, watchThrottled } from '@vueuse/core'
-import { computed, onBeforeMount, useSlots, useTemplateRef } from 'vue'
+import { computed, onBeforeMount, onMounted, useSlots, useTemplateRef } from 'vue'
 import { isNil } from '../../lib/helpers'
 import './sidebar.scss'
 
-const props = withDefaults(defineProps<Props>(), {
-  width: 224,
-  mini: false,
-  appearDelay: 250,
-})
-
-// TODO: refactor props
-// - floaty rename to `float` - appears over content when sidebar is open
-// - appear only does the triggering part, not the floating part
-// - mini: same
-// - remove `width` and control it via CSS variable as well
-// - add variants
-//  - default: like now, with a border
-//  - borderless: no borders
-
 interface Props {
+  /**
+   * Sidebar styling.
+   *
+   * - default: border on the right
+   * - plain: no styling except padding
+   * - card: radius & border around the entire sidebar
+   */
   variant: 'default' | 'plain' | 'card'
-  // /**
-  //  * Width of the sidebar, when it's open (& not minidifed)
-  //  */
-  // width?: number
   /**
    * Controls wether the sidebar is displayed in full size, or a small version.
    */
   mini?: boolean
   /**
-   * If enabled, sidebar opens when user hovers close to where sidebar appears from.
+   * If enabled, sidebar opens when user hovers close to where sidebar appears
+   * from.
    */
   appear?: boolean
   /**
-   * Controls the amount of time user needs to hover the trigger area, for sidebar to show up.
-   *
-   * Default is `250` milliseconds.
+   * Controls the amount of time in milliseconds user needs to hover the trigger
+   * area, for sidebar to show up.
    */
   appearDelay?: number
   /**
    * If enabled, sidebar floats on top of content when opened
    */
   float?: boolean
+
+  /**
+   * Removes automatic scaling / formatting of `<Button />` and `<DropdownItem
+   * />` components when sidebar is is minified.
+   */
+  manualButtons?: boolean
 }
+
+const props = withDefaults(defineProps<Props>(), {
+  mini: false,
+  appearDelay: 250,
+})
 
 const sidebarInner = useTemplateRef('inner')
 const open = defineModel<boolean>({
@@ -51,14 +50,15 @@ const open = defineModel<boolean>({
 })
 const slots = useSlots()
 
-// Defines how far the floaty sidebar appears from the edge of its parent
-// const offset = useCssVar('--vui-sidebar-float-offset', sidebarInner, {
-//   initialValue: '8px',
-// })
-
-const width = useCssVar('--vui-sidebar-width', sidebarInner, {
-  initialValue: '8px',
+const widthFull = useCssVar('--vui-sidebar-width-full', sidebarInner, {
+  initialValue: '224px',
 })
+
+const widthMini = useCssVar('--vui-sidebar-width-mini', sidebarInner, {
+  initialValue: '68px',
+})
+
+const offset = useCssVar('--vui-sidebar-offset', sidebarInner)
 
 // const outerWidth = computed(() => {
 //   const elWidth = sidebarInner.value?.getBoundingClientRect().width
@@ -90,8 +90,8 @@ const width = useCssVar('--vui-sidebar-width', sidebarInner, {
 
 const slotProps = computed(() => ({
   mini: props.mini,
-  floaty: props.floaty,
-  width: props.width,
+  // floaty: props.floaty,
+  // width: props.width,
   open,
   close: () => open.value = false,
 }))
@@ -106,17 +106,29 @@ const { start, stop, isPending } = useTimeoutFn(() => {
 const APPEAR_OFFSET = 32
 
 const sidebarOuter = useTemplateRef('outer')
-const { elementX } = useMouseInElement(sidebarOuter)
 
-onBeforeMount(() => {
-  // If appear is set on mount, but sidebar is open, close i
+const { elementX, elementY, elementHeight } = useMouseInElement(sidebarOuter)
+
+onMounted(() => {
+  // If appear is set on mount, but sidebar is open, close it
   if (props.appear && open.value) {
     open.value = false
   }
 })
 
+// TODO: must convert pix to values, combine and back to px
+const outerWidth = computed(() => {
+  if (props.mini) {
+    return props.variant === 'card'
+      ? widthMini.value + (offset.value * 2)
+      : widthMini.value
+  }
+
+  return props.variant === 'card'
+})
+
 // FIXME: this doeasnt seem to take in account height outside of sidebar
-watchThrottled(elementX, (pos) => {
+watchThrottled([elementX, elementY], (pos) => {
   if (!props.appear || (pos <= APPEAR_OFFSET && pos >= 0 && isPending.value))
     return
 
@@ -143,14 +155,14 @@ watchThrottled(elementX, (pos) => {
 })
 
 onClickOutside(sidebarInner, () => {
-  if (open.value && props.floaty) {
+  if (open.value && props.float) {
     open.value = false
   }
 })
 </script>
 
 <template>
-  <div ref="outer" class="vui-sidebar-outer" :style="{ width: outerWidth }" :class="{ open }">
+  <div ref="outer" class="vui-sidebar-outer" :style="{ width: mini ? widthMini : widthFull }" :class="{ open, `manual-buttons`: manualButtons }">
     <aside
       ref="inner"
       class="vui-sidebar"
